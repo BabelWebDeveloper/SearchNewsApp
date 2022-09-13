dff7ea5e73e8e583ac921329d8fe456719195c35
Przemysław Bąbel <86799399+BabelWebDeveloper@users.noreply.github.com>
2022-09-13 20:47:17 +0200

    Console log record onClick

--- src/aura/ListSearchNews/ListSearchNewsController.js
+++ src/aura/ListSearchNews/ListSearchNewsController.js
@@ -4,4 +4,9 @@
         helper.sendSearchListFromEventToComponent( component, articlesFromHandleSearchListEvent );
         return;
     },
+    selectArticle : function(component, event, helper){
+        var target = component.get("v.searchListFromHandleSearchListEvent")[event.currentTarget.dataset.record],
+        selectedArticle = JSON.stringify(target);
+        console.log(target);
+    },
 })
